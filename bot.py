83c5a01cd04c4dc00a94af4321cde5c86ba044d2
akino-iii <44039974+akino-iii@users.noreply.github.com>
2018-10-11 00:57:45 +0300

    ggl>ynd
    Гугл оказался слегка нерабочим и козлился, пошел к яндексу

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
 import telebot
-from googletrans import Translator
+import requests
 
 bot = telebot.TeleBot('695717859:AAFSt8hkwJMOQuFFnRC6Po_SJa5hmnhJLqQ', threaded=False)
 
 
 @bot.message_handler(content_types=['text'])
 def translate(message):
-    tr = Translator()
-    translated = tr.translate(message.text, dest='en').text
+    text = message.text
+    token = 'trnsl.1.1.20181010T214428Z.ceda94436df92606.b37884e3e65b36d66e9354aaaa0f1b0d12908b9f'
+    url_trans = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+    trans_option = {'key': token, 'lang': 'ru-en', 'text': text}
+    webRequest = requests.get(url_trans, params=trans_option)
+    translated = webRequest.text[36:-3]
     bot.send_message(chat_id=message.chat.id, text=translated)
 
 
